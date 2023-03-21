eb6ecb3ac188d842e744c7715a01cc2236a9e900
aum kendapadi <aum@unc.edu>
2023-03-21 16:42:40 -0400

    added basic probability fractal plants

--- js/prob-fractal-plant.js
+++ js/prob-fractal-plant.js
@@ -1,47 +1,37 @@
-// variables: A B
-// constants: + - "[" "]"
-// axiom: A
-// rules: (A -> B+[[A]-A]-B[-BA]+A), (B -> BB)
-// angle: pi/7
-
 var start = "A";
 var result = start;
 var rules;
-var len = 4;
-var lastState = 0; // assigned starting state of 0
+var len = 5;
 
 rules = {
     "A": [
         {
-            // state 0
-            probability: [0.2, 0.4, 0.4],
-            out: "A[+A]A[-A]A",
+            probability: 0.20,
+            out: "A[+A]A[-A]A"
         },
         {
-            // state 1
-            probability: [0.4, 0.2, 0.4],
-            out: "AA-[-A+A+B]+[+A-A-A]",
+            probability: 0.40,
+            out: "AA-[-A+A+B]+[+A-A-A]"
         },
         {
-            // state 2
-            probability: [0.4, 0.4, 0.2],
-            out: "AA+[+A-A-A]-[-A+A+B]",
+            probability: 0.40,
+            out: "AA+[+A-A-A]-[-A+A+B]"
         }
     ]
 }
 
 function create() {
+    
   var nextText = "";
   for (var i = 0; i < result.length; i++) {
     let r = Math.random();
     let p = 0;
     var current = result.charAt(i);
     if (current == "A") {
-        for (let i = 0; i < 3; i++){
-            p += rules.A[lastState].probability[i]
+        for (let i=0; i < rules.A.length; i++) {
+            p += rules.A[i].probability;
             if (r < p) {
                 nextText += rules.A[i].out;
-                lastState = i;
             }
         }
     } else {
@@ -83,7 +73,7 @@ function setup() {
   createCanvas(1000, 500)
   background(0);
   turtle();
-  createP("l-system + markov chains: fractal plant");
+  createP("l-system + probability: fractal plant");
   var button = createButton("create!");
   button.mousePressed(create);
   createP(start);
